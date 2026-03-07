7b60246e0430f87a9aa02d5cc007a6df9a76c07a
Axel Levy <81815421+Axl-Lvy@users.noreply.github.com>
2026-03-07 19:24:58 +0100

    ci: Add sonar analysis (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,25 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply  false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
     alias(libs.plugins.ktfmt) apply false
+    alias(libs.plugins.sonarqube)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Axl-Lvy_Stockfish-Multiplatform")
+        property("sonar.organization", "axl-lvy")
+        property("sonar.projectName", "Stockfish Multiplatform")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property(
+            "sonar.exclusions",
+            listOf(
+                "stockfish-multiplatform/cpp/stockfish/**",
+                "stockfish-multiplatform/src/**/resources/stockfish/**",
+                "stockfish-multiplatform/src/androidMain/jniLibs/**",
+                "stockfish-multiplatform/src/androidMain/assets/stockfish/**",
+                "stockfish-multiplatform/src/androidHostTest/jniLibs/**",
+                "stockfish-multiplatform/src/androidHostTest/resources/stockfish/**",
+            ).joinToString(","),
+        )
+    }
 }
