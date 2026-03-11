4d33fc6cce26fb3d04a0bfa2d7a881a238a2fa99
Axl <axeldecesson@live.fr>
2026-03-11 19:37:51 +0100

    fix tests and bump version

--- stockfish-multiplatform/karma.config.d/timeout.js
+++ stockfish-multiplatform/karma.config.d/timeout.js
@@ -1,10 +1,10 @@
 // Increase Mocha timeout for long-running concurrency tests
 config.client = config.client || {};
 config.client.mocha = config.client.mocha || {};
-config.client.mocha.timeout = 120000;
+config.client.mocha.timeout = 600000;
 
-// Increase Karma browser-level timeouts so heavy Stockfish computation
-// doesn't trigger "Disconnected, because no message in 30000 ms"
-config.browserNoActivityTimeout = 120000;
-config.browserDisconnectTimeout = 30000;
+// Increase Karma browser-level timeouts so the 108 MB WASM binary
+// has time to compile on slow CI runners before tests start
+config.browserNoActivityTimeout = 600000;
+config.browserDisconnectTimeout = 60000;
 config.browserDisconnectTolerance = 3;
