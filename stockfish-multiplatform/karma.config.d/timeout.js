4f526b65b10f8c6baacdbf09a859c12d3e2a2f8c
Axl-Lvy <axl@activeviam.com>
2026-03-11 14:13:08 +0100

    wasm timeout

--- stockfish-multiplatform/karma.config.d/timeout.js
+++ stockfish-multiplatform/karma.config.d/timeout.js
@@ -2,3 +2,9 @@
 config.client = config.client || {};
 config.client.mocha = config.client.mocha || {};
 config.client.mocha.timeout = 120000;
+
+// Increase Karma browser-level timeouts so heavy Stockfish computation
+// doesn't trigger "Disconnected, because no message in 30000 ms"
+config.browserNoActivityTimeout = 120000;
+config.browserDisconnectTimeout = 30000;
+config.browserDisconnectTolerance = 3;
